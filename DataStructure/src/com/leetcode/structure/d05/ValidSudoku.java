d22a811c5d1fba80e491ed759e9a030fc6dbb03e
kar5873 <707618902@qq.com>
2021-11-24 23:01:49 +0800

    LeetCode数据结构第5天 有效的数独数独

--- DataStructure/src/com/leetcode/structure/d05/ValidSudoku.java
+++ DataStructure/src/com/leetcode/structure/d05/ValidSudoku.java
@@ -6,53 +6,43 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * 请你判断一个 9 x 9 的数独是否有效。只需要 根据以下规则 ，验证已经填入的数字是否有效即可。
+ *
+ * 数字 1-9 在每一行只能出现一次。
+ * 数字 1-9 在每一列只能出现一次。
+ * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。（请参考示例图）
+ *
+ *
+ * 注意：
+ * 一个有效的数独（部分已被填充）不一定是可解的。
+ * 只需要根据以上规则，验证已经填入的数字是否有效即可。
+ * 空白格用 '.' 表示。
+ *
+ *
  * @author Kar
  * @create 2021-11-24 下午4:02
  */
 public class ValidSudoku {
 
 
+    /**
+     *
+     * @param board
+     * @return
+     */
     public boolean isValidSudoku(char[][] board) {
-        // for (int i = 1; i <= 9; i++) {
-        //     map.put(i + "", "");
-        // }
-        int length = board.length;
-        for (int i = 0; i < length; i++) {
-            HashMap<Character, Character> map = new HashMap<>();
-            for (int j = 0; j < length; j++) {
-                if (board[i][j] == '.') {
-                    continue;
-                }
-                if (!map.containsKey(board[i][j])) {
-                    map.put(board[i][j], ' ');
-                } else {
-                    return false;
-                }
-            }
-        }
-        for (int i = 0; i < length; i++) {
-            HashMap<Character, Character> map = new HashMap<>();
-            for (int j = 0; j < length; j++) {
-                if (board[j][i] == '.') {
-                    continue;
-                }
-                if (!map.containsKey(board[j][i])) {
-                    map.put(board[j][i], ' ');
-                } else {
-                    return false;
-                }
-            }
-        }
-        for (int i = 0; i < length; i++) {
-            HashMap<Character, Character> map = new HashMap<>();
-            for (int j = 0; j < length; j++) {
-                if(i/3 == j/3){
-                    if (board[j][i] == '.') {
-                        continue;
-                    }
-                    if (!map.containsKey(board[j][i])) {
-                        map.put(board[j][i], ' ');
-                    } else {
+        int[][] rows = new int[9][9];
+        int[][] columns = new int[9][9];
+        int[][][] subboxes = new int[3][3][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    int index = c - '0' - 1;
+                    rows[i][index]++;
+                    columns[j][index]++;
+                    subboxes[i / 3][j / 3][index]++;
+                    if (rows[i][index] > 1 || columns[j][index] > 1 || subboxes[i / 3][j / 3][index] > 1) {
                         return false;
                     }
                 }
@@ -60,7 +50,4 @@ public class ValidSudoku {
         }
         return true;
     }
-
-
-
 }
