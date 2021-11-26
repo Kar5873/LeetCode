5b1816ca99271a2b616410c94ffe95a3ed69c645
kartian <707618902@qq.com>
2021-11-26 18:01:01 +0800

    LeetCode 数据结构 第7天 链表

---
+++ DataStructure/src/com/leetcode/structure/d07/LinkedListCycle.java
@@ -0,0 +1,32 @@
+package com.leetcode.structure.d07;
+
+import java.util.HashMap;
+
+/**
+ * @author Kar
+ * @create 2021-11-26 上午8:52
+ */
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode temp = head;
+        HashMap<ListNode, Object> map = new HashMap<>();
+        while (temp != null){
+            if(map.containsKey(temp)) {
+                return true;
+            }
+            map.put(temp, null);
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+}
---
+++ DataStructure/src/com/leetcode/structure/d07/MergeTwoSortedLists.java
@@ -0,0 +1,41 @@
+package com.leetcode.structure.d07;
+
+/**
+ * @author Kar
+ * @create 2021-11-26 下午3:02
+ */
+public class MergeTwoSortedLists {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+            return null;
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
