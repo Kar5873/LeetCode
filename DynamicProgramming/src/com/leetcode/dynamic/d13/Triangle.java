36813609c39977beb8f8902d5ed28c5949ec8291
kartian <707618902@qq.com>
2021-12-08 18:12:58 +0800

    LeetCode 动态规划

---
+++ DynamicProgramming/src/com/leetcode/dynamic/d13/Triangle.java
@@ -0,0 +1,16 @@
+package com.leetcode.dynamic.d13;
+
+import java.util.List;
+
+/**
+ * @author Kar
+ * @create 2021-12-08 上午8:43
+ */
+public class Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        for (int i = n - 2; i >= 0; i++) {
+
+        }
+    }
+}
